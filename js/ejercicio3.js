b2cb29c459c93b72d7a9e52c47e7492c4a78e76c
nicoolas-devel <nicoolas23@gmail.com>
2026-07-01 15:32:59 -0300

    Primer commit haciendo los ejercicios del 1 al 3

---
+++ js/ejercicio3.js
@@ -0,0 +1,15 @@
+let numerosMayoresA10 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+let mayores = [];
+
+for (let i = 0; i < numerosMayoresA10.length; i++) {
+
+    if (numerosMayoresA10[i] > 10) {
+
+        mayores.push(numerosMayoresA10[i]);
+
+    }
+
+}
+
+console.log(mayores);
